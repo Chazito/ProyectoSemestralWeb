93bf94dd02c163f7be31755d4e6914696de17ce9
Chazito <f.nunez.acevedo@gmail.com>
2024-05-15 23:28:37 -0400

    PaginaLogin

--- Avance2/verificacion.js
+++ Avance2/verificacion.js
@@ -46,6 +46,53 @@ function handleRegisterForm(event) {
     });
 }
 
+function handleLoginForm(event) {
+    event.preventDefault();
+
+    var username = $("#username").val();
+    var password = $("#password").val();
+
+    //Usuarios de admin prueba
+    const adminUsername = "FrancoN";
+    const adminPass = "12345678";
+
+    //Usuario comun prueba
+    const commonUsername = "AlejandroF";
+    const commonPass = "12345678";
+
+    hideAllErrorMessages();
+
+    if (username == "" || username.length < 6 || username.length > 20) {
+        $("#mensaje1").fadeIn();
+        return false;
+    }
+    if (username != adminUsername && username != commonUsername) {
+        $("#mensaje2").fadeIn();
+        console.log(username);
+        console.log(adminUsername);
+        return false;
+    }
+
+    if (!passwordRegex.test(password)) {
+        $("#mensaje3").fadeIn();
+        return false;
+    }
+    if ((username == adminUsername && password != adminPass) || (username == commonUsername && password != commonPass)) {
+        $("#mensaje4").fadeIn();
+        return false;
+    }
+    else {
+        if (username == adminUsername) {
+            window.location.href = "perfil_de_admin.html";
+            return true;
+        }
+        if (username == commonUsername) {
+            window.location.href = "perfil_de_usuario.html";
+            return true;
+        }
+    }
+}
+
 function hideAllErrorMessages() {
     $("#mensaje1").fadeOut();
     $("#mensaje2").fadeOut();
